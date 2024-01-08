087360267ddec05ef2ec959f0da9a28d7e3fb275
Sergey Drabkin <dsergeyg85@yandex.ru>
2024-01-09 00:35:36 +0300

    add-controllers commit 1_2 style check

--- src/main/java/ru/practicum/shareit/booking/model/BookingState.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingState.java
@@ -1,10 +1,10 @@
 package ru.practicum.shareit.booking.model;
 
 public enum BookingState {
-    WAITING ("Новое бронирование, ожидает одобрения"),
-    APPROVED ("Бронирование подтверждено владельцем"),
-    REJECTED ("Бронирование отклонено владельцем"),
-    CANCELED ("Бронирование отменено создателем");
+    WAITING("Новое бронирование, ожидает одобрения"),
+    APPROVED("Бронирование подтверждено владельцем"),
+    REJECTED("Бронирование отклонено владельцем"),
+    CANCELED("Бронирование отменено создателем");
 
     private final String state;
 
