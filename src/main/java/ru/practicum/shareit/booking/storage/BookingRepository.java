4ac265f2934831de6ddcce9f2cd5d26936f2b28f
Sergey Drabkin <dsergeyg85@yandex.ru>
2024-01-23 15:05:11 +0300

    add-bokings commit 1_9

--- src/main/java/ru/practicum/shareit/booking/storage/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/storage/BookingRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.storage;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
